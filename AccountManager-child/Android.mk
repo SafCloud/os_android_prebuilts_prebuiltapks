e2025daef581f298d8890a20eb8f9a5e3d1c0c9c
srhughes <46631343+SafCloud@users.noreply.github.com>
2020-02-26 11:39:33 -0500

    Update Android.mk

--- AccountManager-child/Android.mk
+++ AccountManager-child/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE := AccountManager
+LOCAL_MODULE := AccountManager-child
 LOCAL_OVERRIDES_PACKAGES := Email
 LOCAL_CERTIFICATE := platform
 
